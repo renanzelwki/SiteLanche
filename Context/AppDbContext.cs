85d7f73467bf12e10320c50b1a686bf48859a6dd
renanzelwki <nanzelwki@gmail.com>
2024-01-09 22:57:07 -0300

    Teste

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SiteLanche.Models;
+
+namespace SiteLanche.Contexto
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+    }
+}
